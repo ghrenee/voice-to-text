ebe9c3e252f5aa09c8f6ce555a6f2a460f61c508
ghrenee <reneecannon@aiexec.tech>
2025-01-29 10:57:09 -0500

    Update text_to_audio.py

--- text_to_audio.py
+++ text_to_audio.py
@@ -2,7 +2,7 @@ from gtts import gTTS
 import os
 
 # Text to be converted to audio
-text = Hello, you've reached eunoiaAI. WE'RE ON A MISSION TO REVOLUTIONIZE HOW BUSINESSES INNOVATE WITH THE POWER OF ARTIFICIAL INTELLIGENCE! To learn more and schedule a personalized demo, please visit us at eunoia-AI-dot-co. Otherwise, leave a message and Renee will respond ASAP.
+text = Hello, you've reached eunoiaAI. WE'RE ON A MISSION TO REVOLUTIONIZE HOW BUSINESSES INNOVATE WITH THE POWER OF ARTIFICIAL INTELLIGENCE! To learn more or schedule a personalized demo, please visit us at eunoia A I dot co. Otherwise, leave a message and Renee will respond ASAP.
 
 # Language in which you want to convert
 language = 'en'
@@ -10,8 +10,8 @@ language = 'en'
 # Passing the text and language to the engine
 speech = gTTS(text=text, lang=language, slow=False)
 
-# Saving the converted audio in a mp3 file named "output.mp3"
-speech.save("output.mp3")
+# Saving the converted audio in a mp3 file named voicemailt.mp3
+speech.save(voicemail.mp3)
 
 # Playing the converted file
-os.system("start output.mp3")
+os.system(start voicemail.mp3)
